c94adfee8b2f74394d0559da4c608e659a476b2f
AnjaleeIrudinee <anjaleeirudinee9396@gmail.com>
2020-10-23 23:22:10 +0530

    add controllers

---
+++ business/Controllers/BranchController.cs
@@ -0,0 +1,33 @@
+﻿using business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace business.Controllers
+{
+    public class BranchController : Controller
+    {
+        private BusinessContext businessContext = new BusinessContext();
+        // GET: Branch
+        public ActionResult Index()
+        {
+            List<Branch> allBranchs = businessContext.Branchs.ToList();
+            return View(allBranchs);
+        }
+        public ActionResult Create()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Branch branch)
+        {
+            businessContext.Branchs.Add(branch);
+            businessContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
